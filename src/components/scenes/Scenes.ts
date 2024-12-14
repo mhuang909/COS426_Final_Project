11a3eda06d0dc7f07eb0065a77c05c6daa26570f
Michael Huang <mhuang909@gmail.com>
2024-12-14 13:47:33 -0500

    two new levels

--- src/components/scenes/Scenes.ts
+++ src/components/scenes/Scenes.ts
@@ -4,10 +4,14 @@ import { sceneData0 } from "../../assets/scenes/scene0";
 import { sceneData1 } from "../../assets/scenes/scene1";
 import { sceneData2 } from "../../assets/scenes/scene2";
 import { sceneData3 } from "../../assets/scenes/scene3";
+import { sceneData4 } from "../../assets/scenes/scene4";
+import { sceneData5 } from "../../assets/scenes/scene5";
 
 export const Scenes = (spritesheet: Spritesheet<SpritesheetData>): { id: string, scene: Scene }[] => [
   { id: "level_0", scene: new Scene(sceneData0, spritesheet) },
   { id: "level_1", scene: new Scene(sceneData1, spritesheet) },
   { id: "level_2", scene: new Scene(sceneData2, spritesheet) },
   { id: "level_3", scene: new Scene(sceneData3, spritesheet) },
+  { id: "level_4", scene: new Scene(sceneData4, spritesheet) },
+  { id: "level_5", scene: new Scene(sceneData5, spritesheet) },
 ]
